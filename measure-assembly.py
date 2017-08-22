11e6f16ce26cc31eb2a5639e4f5073325710f6b9
Miklos Homolya <m.homolya14@imperial.ac.uk>
2017-08-22 13:35:18 +0100

    measure sparse matrix-vector multiplication as well

--- measure-assembly.py
+++ measure-assembly.py
@@ -21,10 +21,6 @@ parser = ArgumentParser(description="""Profile assembly""", add_help=False)
 parser.add_argument("--mode", action="store", default="spectral",
                     help="Which TSFC mode to use?")
 
-parser.add_argument("--output-file", action="store",
-                    default="assembly.csv",
-                    help="Where to put the results")
-
 parser.add_argument("--overwrite", action="store_true", default=False,
                     help="Overwrite existing output?  Default is to append.")
 
@@ -38,13 +34,17 @@ if args.help:
     PETSc.Sys.Print("%s\n" % help)
     sys.exit(0)
 
-filepath = os.path.abspath(args.output_file)
+assembly_path = os.path.abspath("assembly.csv")
+matvec_path = os.path.abspath("matvec.csv")
+
+num_matvecs = 40
 
 
 PETSc.Log.begin()
 
 parloop_event = PETSc.Log.Event("ParLoopExecute")
 assemble_event = PETSc.Log.Event("AssembleMat")
+matmult_event = PETSc.Log.Event("MatMult")
 
 
 simplex_range = list(range(1, 5))
@@ -75,7 +75,7 @@ for problem, tensor in test_cases:
         cellname = 'cube' if tensor else 'simplex'
         PETSc.Sys.Print("%s: %s, degree=%d" % (formname, cellname, degree))
         num_cells = COMM_WORLD.size * max(1e5 / degree**4, 1e8 / (degree + 1)**9)
-        num_cells = max(COMM_WORLD.size, min((1<<27) / (degree + 1)**6, num_cells))
+        num_cells = max(COMM_WORLD.size, min((1 << 27) / (degree + 1)**6, num_cells))
         h = int(floor(cbrt(num_cells / COMM_WORLD.size)))
         w = int(floor(sqrt(num_cells / h)))
         d = int(round(num_cells / (w * h)))
@@ -89,21 +89,28 @@ for problem, tensor in test_cases:
         A = assemble(J, form_compiler_parameters={'mode': args.mode})
         A.force_evaluation()
         Ap = A.petscmat
-        x_size, y_size = Ap.getSize()
-        assert x_size == y_size
-        num_dofs = x_size
+        x, y = Ap.createVecs()
+        assert x.size == y.size
+        num_dofs = x.size
+        Ap.mult(x, y)
         stage = PETSc.Log.Stage("%s(%d) %s" % (formname, degree, cellname))
         with stage:
             with assemble_event:
                 assemble(J, form_compiler_parameters={'mode': args.mode}, tensor=A)
                 A.force_evaluation()
+                Ap = A.petscmat
+            for _ in range(num_matvecs):
+                Ap.mult(x, y)
 
             parloop = parloop_event.getPerfInfo()
             assembly = assemble_event.getPerfInfo()
+            matmult = matmult_event.getPerfInfo()
             assert parloop["count"] == 1
             assert assembly["count"] == 1
+            assert matmult["count"] == num_matvecs
             parloop_time = comm.allreduce(parloop["time"], op=MPI.SUM) / comm.size
             assemble_time = comm.allreduce(assembly["time"], op=MPI.SUM) / comm.size
+            matmult_time = comm.allreduce(matmult["time"], op=MPI.SUM) / (comm.size * num_matvecs)
 
             assembly_overhead = (1 - parloop_time / assemble_time)
             PETSc.Sys.Print("Assembly overhead: %.1f%%" % (assembly_overhead * 100,))
@@ -114,7 +121,7 @@ for problem, tensor in test_cases:
                 header = True
             else:
                 mode = "a"
-                header = not os.path.exists(filepath)
+                header = not os.path.exists(assembly_path)
 
             data = {"num_cells": num_cells,
                     "num_dofs": num_dofs,
@@ -126,4 +133,18 @@ for problem, tensor in test_cases:
                     "assemble_time": assemble_time,
                     "parloop_time": parloop_time}
             df = pandas.DataFrame(data, index=[0])
-            df.to_csv(filepath, index=False, mode=mode, header=header)
+            df.to_csv(assembly_path, index=False, mode=mode, header=header)
+
+            if not args.overwrite:
+                header = not os.path.exists(matvec_path)
+
+            data = {"num_cells": num_cells,
+                    "num_dofs": num_dofs,
+                    "num_procs": comm.size,
+                    "tsfc_mode": args.mode,
+                    "problem": formname,
+                    "cell_type": cellname,
+                    "degree": degree,
+                    "matvec_time": matmult_time}
+            df = pandas.DataFrame(data, index=[0])
+            df.to_csv(matvec_path, index=False, mode=mode, header=header)
