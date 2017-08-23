d8902b85e46f34d3272d368702b89f2cdc9bd8fb
Miklos Homolya <m.homolya14@imperial.ac.uk>
2017-08-23 12:31:39 +0100

    resolve memory leak issue

--- measure-assembly.py
+++ measure-assembly.py
@@ -21,9 +21,6 @@ parser = ArgumentParser(description="""Profile assembly""", add_help=False)
 parser.add_argument("--mode", action="store", default="spectral",
                     help="Which TSFC mode to use?")
 
-parser.add_argument("--overwrite", action="store_true", default=False,
-                    help="Overwrite existing output?  Default is to append.")
-
 parser.add_argument("--help", action="store_true",
                     help="Show help")
 
@@ -60,81 +57,79 @@ test_cases = [
     (form.hyperelasticity, True, 0.1),
     (form.curl_curl, True, 0.1),
 ]
-for problem, tensor, size_factor in test_cases:
+
+
+def run(problem, tensor, size_factor, degree):
     formname = problem.__name__
+    cellname = 'cube' if tensor else 'simplex'
+    PETSc.Sys.Print("%s: %s, degree=%d" % (formname, cellname, degree))
+    num_cells = COMM_WORLD.size * max(1, 1e8 * size_factor / (degree + 1)**7)
+    h = int(floor(cbrt(num_cells / COMM_WORLD.size)))
+    w = int(floor(sqrt(num_cells / h)))
+    d = int(round(num_cells / (w * h)))
+    num_cells = w * d * h
+
+    mesh = ExtrudedMesh(UnitSquareMesh(w, d, quadrilateral=tensor), h)
+    comm = mesh.comm
+    J = problem(mesh, int(degree))
+
+    # Warmup and allocate
+    A = assemble(J, form_compiler_parameters={'mode': args.mode})
+    A.force_evaluation()
+    Ap = A.petscmat
+    x, y = Ap.createVecs()
+    assert x.size == y.size
+    num_dofs = x.size
+    Ap.mult(x, y)
+    stage = PETSc.Log.Stage("%s(%d) %s" % (formname, degree, cellname))
+    with stage:
+        with assemble_event:
+            assemble(J, form_compiler_parameters={'mode': args.mode}, tensor=A)
+            A.force_evaluation()
+            Ap = A.petscmat
+        for _ in range(num_matvecs):
+            Ap.mult(x, y)
+
+        parloop = parloop_event.getPerfInfo()
+        assembly = assemble_event.getPerfInfo()
+        matmult = matmult_event.getPerfInfo()
+        assert parloop["count"] == 1
+        assert assembly["count"] == 1
+        assert matmult["count"] == num_matvecs
+        parloop_time = comm.allreduce(parloop["time"], op=MPI.SUM) / comm.size
+        assemble_time = comm.allreduce(assembly["time"], op=MPI.SUM) / comm.size
+        matmult_time = comm.allreduce(matmult["time"], op=MPI.SUM) / (comm.size * num_matvecs)
+
+        assembly_overhead = (1 - parloop_time / assemble_time)
+        PETSc.Sys.Print("Assembly overhead: %.1f%%" % (assembly_overhead * 100,))
+
+    if COMM_WORLD.rank == 0:
+        header = not os.path.exists(assembly_path)
+        data = {"num_cells": num_cells,
+                "num_dofs": num_dofs,
+                "num_procs": comm.size,
+                "tsfc_mode": args.mode,
+                "problem": formname,
+                "cell_type": cellname,
+                "degree": degree,
+                "assemble_time": assemble_time,
+                "parloop_time": parloop_time}
+        df = pandas.DataFrame(data, index=[0])
+        df.to_csv(assembly_path, index=False, mode='a', header=header)
+
+        header = not os.path.exists(matvec_path)
+        data = {"num_cells": num_cells,
+                "num_dofs": num_dofs,
+                "num_procs": comm.size,
+                "tsfc_mode": args.mode,
+                "problem": formname,
+                "cell_type": cellname,
+                "degree": degree,
+                "matvec_time": matmult_time}
+        df = pandas.DataFrame(data, index=[0])
+        df.to_csv(matvec_path, index=False, mode='a', header=header)
+
+
+for problem, tensor, size_factor in test_cases:
     for degree in (cube_range if tensor else simplex_range):
-        cellname = 'cube' if tensor else 'simplex'
-        PETSc.Sys.Print("%s: %s, degree=%d" % (formname, cellname, degree))
-        num_cells = COMM_WORLD.size * max(1, 1e8 * size_factor / (degree + 1)**7)
-        h = int(floor(cbrt(num_cells / COMM_WORLD.size)))
-        w = int(floor(sqrt(num_cells / h)))
-        d = int(round(num_cells / (w * h)))
-        num_cells = w * d * h
-
-        mesh = ExtrudedMesh(UnitSquareMesh(w, d, quadrilateral=tensor), h)
-        comm = mesh.comm
-        J = problem(mesh, int(degree))
-
-        # Warmup and allocate
-        A = assemble(J, form_compiler_parameters={'mode': args.mode})
-        A.force_evaluation()
-        Ap = A.petscmat
-        x, y = Ap.createVecs()
-        assert x.size == y.size
-        num_dofs = x.size
-        Ap.mult(x, y)
-        stage = PETSc.Log.Stage("%s(%d) %s" % (formname, degree, cellname))
-        with stage:
-            with assemble_event:
-                assemble(J, form_compiler_parameters={'mode': args.mode}, tensor=A)
-                A.force_evaluation()
-                Ap = A.petscmat
-            for _ in range(num_matvecs):
-                Ap.mult(x, y)
-
-            parloop = parloop_event.getPerfInfo()
-            assembly = assemble_event.getPerfInfo()
-            matmult = matmult_event.getPerfInfo()
-            assert parloop["count"] == 1
-            assert assembly["count"] == 1
-            assert matmult["count"] == num_matvecs
-            parloop_time = comm.allreduce(parloop["time"], op=MPI.SUM) / comm.size
-            assemble_time = comm.allreduce(assembly["time"], op=MPI.SUM) / comm.size
-            matmult_time = comm.allreduce(matmult["time"], op=MPI.SUM) / (comm.size * num_matvecs)
-
-            assembly_overhead = (1 - parloop_time / assemble_time)
-            PETSc.Sys.Print("Assembly overhead: %.1f%%" % (assembly_overhead * 100,))
-
-        if COMM_WORLD.rank == 0:
-            if args.overwrite:
-                mode = "w"
-                header = True
-            else:
-                mode = "a"
-                header = not os.path.exists(assembly_path)
-
-            data = {"num_cells": num_cells,
-                    "num_dofs": num_dofs,
-                    "num_procs": comm.size,
-                    "tsfc_mode": args.mode,
-                    "problem": formname,
-                    "cell_type": cellname,
-                    "degree": degree,
-                    "assemble_time": assemble_time,
-                    "parloop_time": parloop_time}
-            df = pandas.DataFrame(data, index=[0])
-            df.to_csv(assembly_path, index=False, mode=mode, header=header)
-
-            if not args.overwrite:
-                header = not os.path.exists(matvec_path)
-
-            data = {"num_cells": num_cells,
-                    "num_dofs": num_dofs,
-                    "num_procs": comm.size,
-                    "tsfc_mode": args.mode,
-                    "problem": formname,
-                    "cell_type": cellname,
-                    "degree": degree,
-                    "matvec_time": matmult_time}
-            df = pandas.DataFrame(data, index=[0])
-            df.to_csv(matvec_path, index=False, mode=mode, header=header)
+        run(problem, tensor, size_factor, degree)
